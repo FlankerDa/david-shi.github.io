aebb974fcda9d129cb122f8b1a2c843f93777a12
FlankerDa <162340363+FlankerDa@users.noreply.github.com>
2024-10-22 12:15:32 -0600

    Update sketch.js

--- Cars Cars Cars!/sketch.js
+++ Cars Cars Cars!/sketch.js
@@ -1,19 +1,19 @@
 // Cars Cars Cars!
 // David Shi
-// Date
+// 2024/10/22
 //
 // Extra for Experts:
-// - describe what you did to take this project "above and beyond"
+// custom traffic
+
 
 
 function setup() {
   createCanvas(windowWidth, windowHeight);
+  let westbound = new vehicle(0,windowHeight/2);
+  let eastbound = new vehicle(windowWidth,windowHeight/2 -75);
 }
 
-function draw() {
-  background(220);
-  drawRoad();
-}
+
 
 // draws the road
 function drawRoad() {
@@ -29,9 +29,37 @@ function drawRoad() {
     rect(x, windowHeight/2, 150, 15);
     
   }
-}  
+}
 
 class vehicle{
-  
-  
+  constructor(x, y, type, color, dir, speed){
+    this.x = x;  this.y = y;
+    this.steps = 10;  this.speed = 5;
+  }
+  display(){  //DISPLAY
+    let veciType = (Math.random() * 2);
+    if (veciType === 0){
+      rect(this.x += this.speed);
+    }
+    if (veciType === 1){
+
+    }
+  }
+
+  move(){
+    this.x += this.speed;     //MOVEMENT
+    this.steps--;  //-=1
+    if(this.steps === 0){
+      this.steps = windowWidth;
+      this.speed *= -1;
+    }       
+  }
+}
+
+function draw() {
+  background(220);
+  drawRoad();
+
+  westbound.display();
+  eastbound.display();
 }
