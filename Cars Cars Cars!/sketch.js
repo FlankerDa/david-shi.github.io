36e0938e8d358523b500027eb7cbfff9d88480fa
davidshi <162340363+FlankerDa@users.noreply.github.com>
2024-10-29 11:58:11 -0600

    10/29 speed run

--- Cars Cars Cars!/sketch.js
+++ Cars Cars Cars!/sketch.js
@@ -1,29 +1,64 @@
 // Cars Cars Cars!
 // David Shi
 // 2024/10/22
-//
-// Extra for Experts:
-// custom traffic
+// custom traffic with trffic lights
 
 
 let eastbound = [];
+let westbound = [];
+let trafficLight;
 
+// set the cars spawn point
 function setup() {
   createCanvas(windowWidth, windowHeight);
 
+  for (let i =0; i < 20; i++) {
+    let yEastbound = random(height / 1.9, height / 3 + height / 2.5);
+    eastbound.push(new Vehicle(random(width), yEastbound, floor(random(2)), 1, random(1, 5)));
+
+    let yWastbound = random(height / 3, height / 2.1);
+    westbound.push(new Vehicle(random(width), yWastbound, floor(random(2)), 0, random(1, 5)));
+  }
+
+
+  trafficLight = new TrafficLight(width / 2, height / 3 - 40);
 }
 
+// draws everything
 function draw() {
   background(220);
   drawRoad();
+  trafficLight.update();
+
+  let canMove = trafficLight.state === "green";
+  
   for (let i = 0; i<eastbound.length; i++){
-    eastbound[i].action();
-    
+    if (canMove) {
+      eastbound[i].move();
+    }
+    eastbound[i].display();
+  }
+
+  for (let i = 0; i< westbound.length; i++){
+    if (canMove){
+      westbound[i].action();
+    }
+    westbound[i].display();
   }
 }
 
+
 function mouseClicked(){
-  eastbound.push(new Vehicle(mouseX, mouseY, 1));
+  let Ey = random(height / 1.9, height / 3 + height / 2.5);
+  let Wy = random(height /3, height / 2.1);
+
+  // when mouse is clicked the cars spawn
+  if (keyIsDown(SHIFT)) {
+    westbound.push(new Vehicle(mouseX, Wy, floor(random(2)), 0, random(1, 5)));
+  } else {
+    eastbound.push(new Vehicle(mouseX, Ey, floor(random(2)), 1, random(1, 5)));
+  }
+
 
 }
 // draws the road
@@ -42,6 +77,7 @@ function drawRoad() {
   }
 }
 
+// the car
 class Vehicle{
   constructor(x, y, type, dir, speed){
     this.x = x;  
@@ -49,20 +85,44 @@ class Vehicle{
     this.dir = dir;
     this.c = color(random(255),random(255),random(255));
     this.type = type;
-    this.nx = speed;
+    this.xSpeed = speed;
   }
-  // contsadahsjrs t jedjy utuj r
 
 
   move() {
-    this.x = this.x + this.nx;
+    this.x += this.xSpeed * (this.dir === 0 ? -1 : 1);
+    if (this.x > width){
+      this.x = 0;
+    }
+    if (this.x < 0){
+      this.x = width;
+    }
   }
 
-  action(){
-    this.display();
+  // random speed
+  speedUp() {
+    if (this.xSpeed < 15) {
+      this.xSpeed += 0.5;
+    }
   }
 
-  display(){  //DISPLAY
+
+
+  // random speed
+  speedDown() {
+    if (this.xSpeed > 0) {
+      this.xSpeed -= 0.5;
+    }
+  }
+
+
+  // color changing
+  changeColor() {
+    this.c = color(random(255), random(255), random(255));
+  }
+
+  display(){
+    fill(this.c);
     if (this.type === 0){
       this.drawCar();
     }
@@ -71,14 +131,81 @@ class Vehicle{
     }
   }
 
+  // draws the car
   drawCar(){
-    fill(this.c);
     rect(this.x,this.y,50,20);
   }
 
+  // draws the truck
   drawTruck(){
-    fill(this.c);
     ellipse(this.x,this.y,50,20);
+  }
+
+  action(){
+    this.move();
+    if (random(1) < 0.01) {
+      this.speedUp();
+    }
+
+    if (random(1) < 0.01) {
+      this.speedDown();
+    }
+    
+    if (random(1) < 0.01) {
+      this.changeColor();
+    }
+    this.display();
+  }
+}
+
+// traffic light
+class TrafficLight {
+  constructor(x, y) {
+    this.x = x;
+    this.y = y;
+    this.state = "green";
+    this.timer = 0;
+  }
+
+
+
+
+  display() {
+    fill(this.state === "green" ? 'green' : 'red');
+    rect(this.x, this.y, 30, 80);
+  }
+
+
+
+
+  toggle() {
+    if (this.state === "green") {
+      this.state = "red";
+      this.timer = 120;
+    }
+  }
+
+
+
+
+  update() {
+    if (this.state === "red") {
+      if (this.timer > 0) {
+        this.timer--;
+      } else {
+        this.state = "green";
+      }
+    }
+    this.display();
+  }
+}
+
+
+
+// when space pressed turn red
+function keyPressed() {
+  if (key === ' ') {
+    trafficLight.toggle();
   }
 }
 
