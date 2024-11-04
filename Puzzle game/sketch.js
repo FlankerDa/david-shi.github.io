ac8f41c975d99ac8f27d72c98382019dfec4f2fd
davidshi <162340363+FlankerDa@users.noreply.github.com>
2024-11-04 12:17:35 -0600

    puzzle game

--- Puzzle game/sketch.js
+++ Puzzle game/sketch.js
@@ -7,21 +7,26 @@ let NUM_ROWS = 4;
 let NUM_COLS = 5;
 let rectWidth, rectHeight;
 let currentRow, currentCol;
-let gridData = [[0,0,0,0,0],
-  [0,0,0,0,0],
-  [0,255,0,0,0],
-  [255,255,255,0,0]];
+let gridData = [[0, 0, 0, 0, 0],
+[0, 0, 0, 0, 0],
+[0, 255, 0, 0, 0],
+[255, 255, 255, 0, 0]];
 
 
-function RandomizedStartingArrangement(){
-
+function RandomizedStartingArrangement() {
+  for (let i = 0; i < NUM_COLS; i++) {
+    for (let j = 0; j < NUM_ROWS; j++) {
+      grid[NUM_COLS][NUM_ROWS] = random([0, 255]);
+    }
+  }
 }
 
 function setup() {
   // Determine the size of each square. Could use windowHeight,windowHeight  for Canvas to keep a square aspect ratio
   createCanvas(windowWidth, windowHeight);
-  rectWidth = width/NUM_COLS;
-  rectHeight = height/NUM_ROWS;
+  rectWidth = width / NUM_COLS;
+  rectHeight = height / NUM_ROWS;
+  RandomizedStartingArrangement();
 }
 
 function draw() {
@@ -29,12 +34,13 @@ function draw() {
   determineActiveSquare();   //figure out which tile the mouse cursor is over
   drawGrid();  //render the current game board to the screen (and the overlay)
   winCondition();
+
 }
 
 
 
 
-function mousePressed(){
+function mousePressed() {
   // cross-shaped pattern flips on a mouseclick. Boundary conditions are checked within the flip function to ensure in-bounds access for array
 
   if (keyCode === SHIFT && keyIsPressed) {
@@ -42,66 +48,68 @@ function mousePressed(){
   }
   else {
     flip(currentCol, currentRow);
-    flip(currentCol-1, currentRow);
-    flip(currentCol+1, currentRow);
-    flip(currentCol, currentRow-1);
-    flip(currentCol, currentRow+1);
-  
+    flip(currentCol - 1, currentRow);
+    flip(currentCol + 1, currentRow);
+    flip(currentCol, currentRow - 1);
+    flip(currentCol, currentRow + 1);
+
   }
 }
 
 
-function flip(col, row,){
+function flip(col, row,) {
   // given a column and row for the 2D array, flip its value from 0 to 255 or 255 to 0
   // conditions ensure that the col and row given are valid and exist for the array. If not, no operations take place.
-  if (col >= 0 && col < NUM_COLS ){
-    if (row >= 0 && row < NUM_ROWS){
+  if (col >= 0 && col < NUM_COLS) {
+    if (row >= 0 && row < NUM_ROWS) {
       if (gridData[row][col] === 0) gridData[row][col] = 255;
-      else gridData[row][col] = 0;
-    }
+      else {
+        gridData[row][col] = 0;
 
-  }
-}
 
-function determineActiveSquare(){
-  // An expression to run each frame to determine where the mouse currently is.
-  currentRow = int(mouseY / rectHeight);
-  currentCol = int(mouseX / rectWidth);
-}
+      】
 
-function drawGrid(){
-  // Render a grid of squares - fill color set according to data stored in the 2D array
-  for (let x = 0; x < NUM_COLS ; x++){
-    for (let y = 0; y < NUM_ROWS; y++){
-      fill(gridData[y][x]); 
-      rect(x*rectWidth, y*rectHeight, rectWidth, rectHeight);
     }
   }
-}
 
-function winCondition() {
-  let firstValue = gridData[0][0];
-  let allSame = true;
+  function determineActiveSquare() {
+    // An expression to run each frame to determine where the mouse currently is.
+    currentRow = int(mouseY / rectHeight);
+    currentCol = int(mouseX / rectWidth);
+  }
 
-
-  for (let row = 0; row < NUM_ROWS; row++) {
-    for (let col = 0; col < NUM_COLS; col++) {
-      if (gridData[row][col] !== firstValue) {
-        allSame = false;
-        break;
+  function drawGrid() {
+    // Render a grid of squares - fill color set according to data stored in the 2D array
+    for (let x = 0; x < NUM_COLS; x++) {
+      for (let y = 0; y < NUM_ROWS; y++) {
+        fill(gridData[y][x]);
+        rect(x * rectWidth, y * rectHeight, rectWidth, rectHeight);
       }
     }
-    if (!allSame) {break;}
   }
 
+  function winCondition() {
+    let firstValue = gridData[0][0];
+    let allSame = true;
 
-  if (allSame) {
-    textSize(32);
-    fill(0, 255, 0);
-    textAlign(CENTER, CENTER);
-    text("You Win", width / 2, height / 2);
+
+    for (let row = 0; row < NUM_ROWS; row++) {
+      for (let col = 0; col < NUM_COLS; col++) {
+        if (gridData[row][col] !== firstValue) {
+          allSame = false;
+          break;
+        }
+      }
+      if (!allSame) { break; }
+    }
+
+
+    if (allSame) {
+      textSize(32);
+      fill(0, 255, 0);
+      textAlign(CENTER, CENTER);
+      text("You Win", width / 2, height / 2);
+    }
   }
-}
-
 
 
