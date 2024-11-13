8b7ad6fedf3bcb284f7a382e69ced7cc6bc5d972
davidshi <162340363+FlankerDa@users.noreply.github.com>
2024-11-13 12:07:43 -0600

    puzzle game

--- Puzzle game/sketch.js
+++ Puzzle game/sketch.js
@@ -69,23 +69,14 @@ function mousePressed() {
 }
 
 
-function flip(col, row,) {
+function flip(col, row) {
   // given a column and row for the 2D array, flip its value from 0 to 255 or 255 to 0
   // conditions ensure that the col and row given are valid and exist for the array. If not, no operations take place.
-  if (col >= 0 && col < NUM_COLS) {
-    if (row >= 0 && row < NUM_ROWS) {
-      if (gridData[row][col] === 0) {
-        gridData[row][col] = 255;
-      }
-      else {
-        gridData[row][col] = 0;
-
-
-      }
-
-    }
+  if (col >= 0 && col < NUM_COLS && row >= 0 && row < NUM_ROWS) {
+    gridData[row][col] = gridData[row][col] === 0 ? 255 : 0;
   }
 }
+
 function determineActiveSquare() {
   // An expression to run each frame to determine where the mouse currently is.
   currentRow = int(mouseY / rectHeight);
